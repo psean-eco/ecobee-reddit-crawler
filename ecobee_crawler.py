db89c9492b4816413b073e3a24e16632e6e2f6a8
Prasanth Pillai <prasanthp@ecobee.com>
2017-09-03 14:42:52 -0400

    added slack integration via post_to_slack method webhook_url added to contstants file

--- ecobee_crawler.py
+++ ecobee_crawler.py
@@ -2,13 +2,13 @@ import praw
 import prawcore.exceptions
 import reddit_constants
 import requests
-import slackclient
 
 client_id = reddit_constants.client_id
 client_secret = reddit_constants.client_secret
 username = reddit_constants.username
 password = reddit_constants.password
 user_agent = reddit_constants.user_agent
+webhook_url = reddit_constants.webhook
 
 def post_print(reddit):
     arr =[]
@@ -16,7 +16,7 @@ def post_print(reddit):
     upvotes = "Upvotes:{votes}"
     comments = "Comments:{comments}"
     link = "Link to Thread:{link} "
-    posts = reddit.subreddit('ecobee').hot(limit=10)
+    posts = reddit.subreddit('ecobee').new(limit=10)
     for submission in posts:
         story={}
         story["title"] = submission.title
@@ -24,12 +24,23 @@ def post_print(reddit):
         story["link"] = "https://www.reddit.com"+ submission.permalink
         story ["totalComments"] = submission.num_comments
         arr.append(story)
+        post_to_slack(story)
+    print(arr)
 
-    post_to_slack(arr)
-#todo create method to post to slack/ask alan ?
-def post_to_slack(story_array):
-    #requests.post()
-    print(story_array)
+
+def post_to_slack(story):
+    payload={}
+    link_text = "<{}| Read More>".format(story['link'])
+    payload['txt'] = "--------------------"+"\n"+"_"+ ":triangular_flag_on_post: "+ story['title'] + "_" + " " +" " + link_text  + "\n" +  "Upvotes: {}".format(story['upvotes'])
+    response = requests.post(
+        webhook_url, json=payload,
+        headers={'Content-Type': 'application/json'}
+    )
+    if response.status_code != 200:
+        raise ValueError(
+            'Request to slack returned an error: {} with error code: {code}'.format(response.text,code=response.status_code)
+        )
+
 
 def main():
     try:
